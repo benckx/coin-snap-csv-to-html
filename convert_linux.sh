0bc0e0710c072b4a6c701916d2b1be791b2bce8b
benckx <benoit.vleminckx@protonmail.com>
2026-02-23 08:38:23 +0100

    fix in helper script

--- convert_linux.sh
+++ convert_linux.sh
@@ -3,4 +3,4 @@
 # Script adapted to the specifics organization of my Linux Mint laptop
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 source "$SCRIPT_DIR/convert_common.sh"
-convert_coinsnap /Project/benckx.me/coins/index.html
+convert_coinsnap ~/Projects/benckx.me/coins/index.html
